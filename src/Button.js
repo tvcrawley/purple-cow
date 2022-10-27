a5d9e3c69f9c3acc5a085b7abcab8eb45af4fb5f
Toni Langley <tonia.langley@gmail.com>
2022-10-27 16:03:32 -0400

    add api request on button click

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,11 @@
 function Button() {
+  const handleButtonClick = () => {
+    fetch("https://api.countapi.xyz/hit/mysite.com/1ccb732e-b55a-4404-ad3f-0f99c02fe44e")
+    .then(res => res.json())
+    .then(result => console.log(result.value))
+  }
   return (
-    <button className="Button">
+    <button className="Button" onClick={handleButtonClick}>
       Click Me
     </button>
   );
