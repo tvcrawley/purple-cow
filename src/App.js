745a9b7ecc9da300ba85a6bb31f8263a9a860f76
Toni Langley <tonia.langley@gmail.com>
2022-10-27 16:11:57 -0400

    set hit count to state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Button from './Button'
+import {useState} from 'react';
 
 function App() {
+  const [hitCount, setHitCount] = useState(0);
   return (
     <div className="App">
-      <Button />
+      <Button setHitCount={setHitCount} />
+      <p>The hit count is {hitCount}.</p>
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,8 @@
-function Button() {
+function Button({setHitCount}) {
   const handleButtonClick = () => {
     fetch("https://api.countapi.xyz/hit/mysite.com/1ccb732e-b55a-4404-ad3f-0f99c02fe44e")
     .then(res => res.json())
-    .then(result => console.log(result.value))
+    .then(result => setHitCount(result.value))
   }
   return (
     <button className="Button" onClick={handleButtonClick}>
